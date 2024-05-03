cd9a308e9b2d3d4110a1ed91b72efde8e3ad7268
Askur Rahman <imaskurrahman@gmail.com>
2024-05-03 19:48:29 +0600

    api gateway

---
+++ api-gateway/src/index.ts
@@ -0,0 +1,60 @@
+import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
+import morgan from 'morgan'
+import { configureRoutes } from './utils';
+
+
+
+dotenv.config();
+
+const app = express();
+
+//security middleware
+app.use(helmet());
+
+//Rate limiting middleware
+const limiter = rateLimit({
+    windowMs: 15* 60 *1000, //15 minutes
+    max: 3, //limit each IP to 100 requests per windowMs
+    handler: (_req, res) => {
+        res
+        .status(429)
+        .json({ message: "Too many requests,please try again later"})
+
+    } 
+})
+app.use('/api', limiter);
+
+//request logger
+app.use(morgan('dev'))
+app.use(express.json());
+
+//TODO Auth middleware
+
+// routes
+configureRoutes(app);
+
+// health check
+app.get('/health', (_req, res) => {
+    res.json({message: 'API Gateway is running'})
+})
+
+//404 handler
+app.use((_req, res) => {
+    res.status(404).json({ message: 'Not Found'})
+})
+
+
+
+// Error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Internal server error" });
+});
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+    console.log(`API Gateway is running on port ${PORT}`)
+})
---
+++ api-gateway/src/utils.ts
@@ -0,0 +1,51 @@
+import { Express, Request, Response } from "express";
+import config from './config.json';
+import axios from "axios";
+
+export const createHandler = (hostname: string, path: string, method: string) => {
+    return async (req: Request, res: Response) => {
+        try {
+            let url = `${hostname}${path}`;
+            req.params && Object.keys(req.params).forEach(param => {
+                url = url.replace(`:${param}`, req.params[param]);
+            })
+
+            const { data } = await axios({
+                method,
+                url, 
+                data: req.body,
+                headers: {
+                    origin: 'http://localhost:8081',
+                }
+            });
+
+            res.json(data);
+        } catch (error) {
+            if (error instanceof axios.AxiosError) {
+                return res.status(error.response?.status || 500).json(error.response?.data);
+            }
+            console.log(error);
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
+    };
+};
+
+export const configureRoutes = (app: Express) => {
+    Object.entries(config.services).forEach(([_, service]) => {
+        const hostname = service.url;
+        service.routes.forEach((route) => {
+            route.methods.forEach((method) => {
+                const handler = createHandler(hostname, route.path, method);
+                const endpoint = `/api${route.path}`;
+                console.log('Endpoint', endpoint);
+                // Use type assertion to assure TypeScript that `method` is one of the valid HTTP methods
+                (app as any)[method](endpoint, handler);
+            });
+        });
+    });
+};
+
+
+
+
+
